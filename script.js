f8ef42dd6e42cc890fa4140f9f69a5e38bd80a0f
rishavchakravarty <145301473+rishavchakravarty@users.noreply.github.com>
2023-12-31 21:53:05 +0300

    Release: v 1.00
    Revamped the entire website, added portfolio.

--- script.js
+++ script.js
@@ -1,56 +1,80 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Smooth Scrolling
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Other JavaScript functionalities can be added here.
+});
+
 // Smooth scrolling for navigation links
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-        });
+        const href = this.getAttribute('href');
+        
+        // Check if the link is internal
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            const targetId = href.substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        }
+        // If not, the default action (navigating to a new page) will occur
     });
 });
 
 // "Back to top" button functionality
 const backToTopBtn = document.getElementById('back-to-top');
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 100) {
-        backToTopBtn.style.display = 'block';
-    } else {
-        backToTopBtn.style.display = 'none';
-    }
-});
-backToTopBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
-document.addEventListener('scroll', () => {
-    const elements = document.querySelectorAll('[data-aos="fade-up"]');
-    elements.forEach(el => {
-      if (el.getBoundingClientRect().top < window.innerHeight) {
-        el.style.opacity = 1;
-        el.style.transform = 'translateY(0)';
-      }
-    });
-  });
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-      // Load More Content Here
-    }
-});
-document.getElementById('theme-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
-document.addEventListener("scroll", function() {
-    const timelineItems = document.querySelectorAll('.timeline-item');
-    timelineItems.forEach(function(item) {
-        const rect = item.getBoundingClientRect();
-        if (rect.top < window.innerHeight && rect.bottom > 0) {
-            item.classList.add('in-view');
+
+if (backToTopBtn) {
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 100) {
+            backToTopBtn.style.display = 'block';
         } else {
-            item.classList.remove('in-view');
+            backToTopBtn.style.display = 'none';
         }
     });
+
+    backToTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+
+// Dropdown functionality
+document.querySelectorAll('.dropbtn').forEach(button => {
+    button.addEventListener('click', function() {
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+});
+
+// Close dropdowns when clicking outside
+document.addEventListener('click', function(event) {
+    var isClickInsideAnyDropdown = Array.from(document.querySelectorAll('.dropdown')).some(dropdown => dropdown.contains(event.target));
+
+    if (!isClickInsideAnyDropdown) {
+        document.querySelectorAll('.dropdown-content').forEach(dropdownContent => {
+            dropdownContent.style.display = 'none';
+        });
+    }
 });
